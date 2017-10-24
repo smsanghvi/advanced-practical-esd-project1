90be3bb282f080c5b9d20c13011490be260f0cb6
smsanghvi <snehal.sanghvi@colorado.edu>
2017-10-24 16:47:52 -0600

    folder structure

---
+++ headers/messaging.h
@@ -0,0 +1,20 @@
+/******************************************************
+*   File: messaging.h
+*
+​* ​ ​ Copyright​ ​ (C)​ ​ 2017​ ​ by​ ​ Snehal Sanghvi
+​*
+​* ​ ​ Redistribution,​ ​ modification​ ​ or​ ​ use​ ​ of​ ​ this​ ​ software​ ​ in​ ​ source​ ​ or​ ​ binary
+​* ​ ​ forms​ ​ is​ ​ permitted​ ​ as​ ​ long​ ​ as​ ​ the​ ​ files​ ​ maintain​ ​ this​ ​ copyright.​ ​ Users​ ​ are
+​* ​ ​ permitted​ ​ to​ ​ modify​ ​ this​ ​ and​ ​ use​ ​ it​ ​ to​ ​ learn​ ​ about​ ​ the​ ​ field​ ​ of​ ​ embedded
+​* ​ ​ software.​ ​ Snehal Sanghvi​ ​ and​ ​ the​ ​ University​ ​ of​ ​ Colorado​ ​ are​ ​ not​ ​ liable​ ​ for
+​* ​ ​ any​ ​ misuse​ ​ of​ ​ this​ ​ material.
+*
+*
+*   Authors: Snehal Sanghvi and Rishi Soni
+*   Date Edited: 9 Nov 2016
+*
+*   Description: Header file for messaging
+
+*
+********************************************************/
+
