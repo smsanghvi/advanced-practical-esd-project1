e33e560187d927b1cfd5c4ca58df4b05cfb6b031
smsanghvi <snehal.sanghvi@colorado.edu>
2017-11-04 14:12:37 -0600

    Added in copyrights

--- sources/light_sensor.c
+++ sources/light_sensor.c
@@ -1,3 +1,23 @@
+/******************************************************
+*   File: tasks.c
+*
+​* ​ ​ The MIT License (MIT)
+*   Copyright (C) 2017 by Snehal Sanghvi and Rishi Soni
+*   Redistribution,​ ​ modification​ ​ or​ ​ use​ ​ of​ ​ this​ ​ software​ ​ in​ ​ source​ ​ or​ ​ binary
+​* ​ ​ forms​ ​ is​ ​ permitted​ ​ as​ ​ long​ ​ as​ ​ the​ ​ files​ ​ maintain​ ​ this​ ​ copyright.​ ​ Users​ ​ are
+​* ​ ​ permitted​ ​ to​ ​ modify​ ​ this​ ​ and​ ​ use​ ​ it​ ​ to​ ​ learn​ ​ about​ ​ the​ ​ field​ ​ of​ ​ embedded
+​* ​ ​ software.​ ​ The authors​ and​ ​ the​ ​ University​ ​ of​ ​ Colorado​ ​ are​ ​ not​ ​ liable​ ​ for
+​* ​ ​ any​ ​ misuse​ ​ of​ ​ this​ ​ material.
+*
+*
+*   Authors: Snehal Sanghvi and Rishi Soni
+*   Date Edited: 4 Nov 2017
+*
+*   Description: Source file for light sensor
+*
+*
+********************************************************/
+
 #include "i2c.h"
 #include <stdint.h>
 #include <stdlib.h>
